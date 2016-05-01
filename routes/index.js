a1e43ee019fe21495ddf04b53e1536f5936d2487
Shubhi Jain <shubhijain@caltech.edu>
2016-05-01 06:36:59 -0700

    landing page biznitch

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moodify' });
+  res.render('landing');
 });
 
 module.exports = router;
