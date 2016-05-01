a8215e6d14e4f835871e1af7c3fca642a08ee188
Jesse Zhou <jesse.cy.zhou@gmail.com>
2016-04-30 17:53:24 -0700

    webhook post created

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET webhook listing. */
 router.get('/', function (req, res) {
   if (req.query['hub.verify_token'] === process.env.FACEBOOK_VALIDATION_TOKEN) {
     res.send(req.query['hub.challenge']);
   }
 
-  res.send('Error, wrong validation token. ' + req.query['hub.verify_token']);
+  res.send('Error, wrong validation token.');
 })
+
+router.post('/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
 
 module.exports = router;
