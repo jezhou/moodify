f5e263cf83463e1bbaaacce5517d9a4225c69a3b
Jesse Zhou <jesse.cy.zhou@gmail.com>
2016-04-30 18:02:20 -0700

    testing rudimentary responses

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -24,4 +24,27 @@ router.post('/', function (req, res) {
   res.sendStatus(200);
 });
 
+var token = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 module.exports = router;
