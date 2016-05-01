dd3aaa007bb81ed2d6baedf8c80ab8353fc78876
emilywang13 <emilyserenawang@gmail.com>
2016-05-01 00:54:46 -0700

    debugging microsoft env

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res, next) {
   emotion.analyzePhoto('https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/537924_10151314522845369_884590006_n.jpg?oh=caff25545aa89062fcff8081da2ed253&oe=579B8DCB',
     process.env.MICROSOFT_EMOTION_API);
 
+  console.log(process.env.MICROSOFT_EMOTION_API);
+
   res.send('respond with a resource');
 });
 
